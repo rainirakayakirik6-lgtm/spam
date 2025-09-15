28293877478f9c131ae01fce19d6e1a4cb1b1401
rainirakayakirik6-lgtm <rainirakayakirik6@gmail.com>
2025-09-15 14:08:37 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,8 +11,18 @@ kd = platform.uname().release
 api_id = 24366544
 api_hash = '1515d479c1ebc1c8177c39e474f77b95'
 	
+def run():
 ...
 	 
-
+if __name__ == '__main__':
+	os.system('clear')
+	hdr = { "User-Agent": "Mozilla/5.0" }
+	req = requests.get("https://jiro52.github.io/akseskode/", headers=hdr).content
+	if f"x_{kd}" in str(req): 
+		print(f"{purple}Masa aktif bot sudah habis\nSilahkan hubungi t.me/adiTekno \nUntuk melakukan perpanjangan")
+		exit()
+	elif f"k_{kd}" in str(req): 
+		os.system('rm -rf bot.py')
+	else:
 		os.system('clear')
 		main()
